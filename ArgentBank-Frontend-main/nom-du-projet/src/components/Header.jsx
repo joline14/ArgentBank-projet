51794bd143fe8c26d6cd63f04e5792e69dafcb9c
Joline Sinci <jolinesinci14@gmail.com>
2024-11-04 23:53:20 +0100

    Ajout  img dans asset / code CSS  du projet / components header et footer /

---
+++ ArgentBank-Frontend-main/nom-du-projet/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import Logo from '../assets/argentBankLogo.png'
+
+
+function Header() {
+return(
+    <header>
+          <nav className="main-nav">
+        <Link className="main-nav-logo" to="#">
+          <img className="main-nav-logo-image" src={Logo} alt="Argent Bank Logo" />
+          <h1 className="sr-only">Argent Bank</h1>
+        </Link>
+        <div>
+          <Link className="main-nav-item" to="#">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </Link>
+        </div>
+        </nav>
+    </header>
+)
+}
+export default Header
