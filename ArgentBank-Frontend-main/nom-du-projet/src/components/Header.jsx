698159f252178cd74ddbc51c119cf4bce3d13c14
Joline Sinci <jolinesinci14@gmail.com>
2024-11-09 23:45:14 +0100

    ajout de l'état de connexion et Màj de l'affichage Sign In/Sign Out dans le header

--- ArgentBank-Frontend-main/nom-du-projet/src/components/Header.jsx
+++ ArgentBank-Frontend-main/nom-du-projet/src/components/Header.jsx
@@ -1,8 +1,32 @@
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../assets/argentBankLogo.png'
 
-
 function Header() {
+//État pour vérifier si l'utilisateur est connecté et le profil utilisateur
+const [isAuthenticated, setIsAuthenticated] = useState(false);
+const [userProfile, setUserProfile] = useState(null);
+
+// Vérifie s'il y a un token au chargement de la page
+useEffect(() => {
+  const token = localStorage.getItem('authToken');
+  if (token) {
+    setIsAuthenticated(true); // Met à jour l'état de connexion
+    const storedProfile = JSON.parse(localStorage.getItem('userProfile'));
+    if (storedProfile) {
+      setUserProfile(storedProfile); // Charge le profil utilisateur
+    }
+  }
+}, []);
+
+// Fonction pour gérer la déconnexion
+const handleSignOut = () => {
+  localStorage.removeItem('authToken'); // Supprime le token
+  localStorage.removeItem('userProfile'); // Supprime le profil
+  setIsAuthenticated(false); // Met à jour l'état de connexion
+  setUserProfile(null); // Réinitialise le profil
+};
+
 return(
     <header>
           <nav className="main-nav">
@@ -10,14 +34,31 @@ return(
           <img className="main-nav-logo-image" src={Logo} alt="Argent Bank Logo" />
           <h1 className="sr-only">Argent Bank</h1>
         </Link>
-        <div>
+
+        {/* Affichage conditionnel des liens */}
+        {isAuthenticated ? (
+          // Si l'utilisateur est connecté
+          <div className="main-nav-ctaItem">
+            <Link className="main-nav-item" to="./profile">
+              <i className="fa fa-user-circle"></i>
+              {userProfile ? userProfile.userName : 'Load'}
+            </Link>
+            <Link className="main-nav-item" to="./" onClick={handleSignOut}>
+              <i className="fa fa-sign-out"></i>
+              Sign Out
+            </Link>
+          </div>
+        ) : (
+          // Si l'utilisateur n'est pas connecté
+        <div className="main-nav-ctaItem">
           <Link className="main-nav-item" to="/Login">
             <i className="fa fa-user-circle"></i>
             Sign In
           </Link>
         </div>
+        )}
         </nav>
     </header>
-)
+);
 }
 export default Header
