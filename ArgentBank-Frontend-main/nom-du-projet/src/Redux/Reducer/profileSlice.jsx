bea53484d9b37b5f6b5da4f6c7933f7d8fe0e607
Joline Sinci <jolinesinci14@gmail.com>
2024-11-17 14:17:52 +0100

    Création du reducers(slices): profileSlice

---
+++ ArgentBank-Frontend-main/nom-du-projet/src/Redux/Reducer/profileSlice.jsx
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const profileSlice= createSlice({
+    name: 'user',
+    // État initial du slice d'authentification
+initialState: {
+    firstName: '',
+    lastName: '',
+    userName: '',
+    email: '',
+  },
+    reducers:{
+        setProfile:(state, action)=>{
+            state.firstName= action.payload.body.firstName;
+            state.lastName = action.payload.body.lastName;
+            state.userName = action.payload.body.userName;
+            state.email = action.payload.body.email;
+        },
+        // Action 'updateUserName' : Mets à jour le champ 'userName' du profil avec la valeur fournie dans l'action
+        updateUserName: (state, action) => {
+        state.userName = action.payload;
+        },
+    }    
+})
+export const { setProfile, updateUserName } = profileSlice.actions;
+export default profileSlice.reducer;
