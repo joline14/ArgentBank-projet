841e7d4f64b6d2066b61b18f1665082cc419035a
Joline Sinci <jolinesinci14@gmail.com>
2024-11-28 12:53:16 +0100

    Gestion de la navigation dans l'application

--- ArgentBank-Frontend-main/nom-du-projet/src/App.jsx
+++ ArgentBank-Frontend-main/nom-du-projet/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 import Header from './components/Header'
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -6,13 +8,16 @@ import Login from "./pages/Login";
 import User from "./pages/User"
 
 function App() {
+
+  const isAuthenticated = useSelector((state) => state.authentication.isAuthenticated); 
+
 return(
   <BrowserRouter>
       <Header />
       <Routes>
       <Route path="/" element={<Home/>} />
-      <Route path="/Login" element={<Login/>} />
-      <Route path="/User" element={<User/>} />
+      <Route path="/Login" element={isAuthenticated ? <Navigate to={"/User"} /> : <Login/>} /> 
+      <Route path="/User" element={isAuthenticated ? <User/> : <Navigate to={"/Login"} /> } /> 
       </Routes>
       <Footer/>
     </BrowserRouter>
